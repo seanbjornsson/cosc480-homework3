f983478c652158766186c77dbc4b6e913355b0c9
sbjornsson <sbjornsson@colgate.edu>
2013-02-27 21:15:09 -0500

    finished

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -22,17 +22,14 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings 
-	When I check the following ratings: PG R
-	When I uncheck the following ratings: G PG-13
+	When I check the following ratings: PG, R
+	When I uncheck the following ratings: G, PG-13
 	When I press "Refresh"
 	Then I should see movies with the following ratings: PG R
 	Then I should not see movies with the following ratings: G PG-13
-	
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
 
 Scenario: all ratings selected
-  # see assignment
+	When I check all ratings
+	Then I should see all of the movies
+  
+# see assignment
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -18,7 +18,7 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-	rating_list.split(' ').each do |rating|
+	rating_list.gsub(/,/, ' ').split(' ').each do |rating|
 		if uncheck == 'un' 
 			step %{I uncheck "ratings_#{rating}"}
 		else
@@ -27,8 +27,21 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
 	end
 end
 
+When /I check all ratings/ do
+	Movie.all_ratings.each do |rating|
+		step %{I check "ratings_#{rating}"}
+	end
+end
+
+Then /I should see all of the movies/ do
+	rows = (page.all('table#movies tr').count-1)
+ print "number of rows in the table = #{rows}\n"
+	print "number of movies in db = #{Movie.count}"
+	(page.all('table#movies tr').count-1).should == Movie.count
+end
+
 Then /I should (not )?see movies with the following ratings: (.*)/ do |show, rating_list|
-	Movie.find_all_by_rating(rating_list.split(' ')).each do |movie|
+	Movie.find_all_by_rating(rating_list.gsub(/,/, ' ').split(' ')).each do |movie|
 		if show == 'not '	
 			step %{I should not see "#{movie[:title]}"}
 		else
@@ -36,3 +49,24 @@ Then /I should (not )?see movies with the following ratings: (.*)/ do |show, rat
 		end
 	end
 end
+
+Then /the movies should be sorted (alphabetically|by release date)/ do |order|
+	if order == 'alphabetically'
+		sort = 'title'
+	else
+		sort = 'release_date'
+	end
+	movies = Movie.order(sort).select("title")
+	0.upto(movies.length-2) do |i|
+		print "movies being compared: #{movies[i]["title"]} and #{movies[i+1]["title"]}\n"
+		step %{I should see "#{movies[i]["title"]}" before "#{movies[i+1]["title"]}"}
+	end
+end
+
+
+
+
+
+
+
+
